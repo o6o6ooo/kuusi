bf4722876d527c618d8bec534ec0ec7116e64852
Sakura Wallace <066sakura@gmail.com>
2025-07-19 15:26:40 +0100

    updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigation/RootNavigator';
+import { DefaultTheme, NavigationContainer, DarkTheme as NavigationDarkTheme } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
+import { DarkTheme, LightTheme } from './src/constants/theme'; // テーマ定義をインポート
+import RootNavigator from './src/navigation/RootNavigator'; // StackNavigatorをまとめたコンポーネント
 
 export default function App() {
+    const scheme = useColorScheme();
+    const theme = scheme === 'dark'
+        ? { ...NavigationDarkTheme, colors: { ...NavigationDarkTheme.colors, ...DarkTheme } }
+        : { ...DefaultTheme, colors: { ...DefaultTheme.colors, ...LightTheme } };
+
     return (
-        <NavigationContainer>
+        <NavigationContainer theme={theme}>
             <RootNavigator />
         </NavigationContainer>
     );
