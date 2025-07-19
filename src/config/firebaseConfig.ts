ce2690c1a428ba88ea2da211ef7352c2574469b3
Sakura Wallace <066sakura@gmail.com>
2025-07-19 22:32:05 +0100

    created src/config/firebaseConfig.ts

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAcTyKsmLxrcc-YkWdLf_ChElevPlhJ0l0",
+    authDomain: "kuusi-8e573.firebaseapp.com",
+    projectId: "kuusi-8e573",
+    storageBucket: "kuusi-8e573.appspot.com",
+    messagingSenderId: "898290307420",
+    appId: "1:898290307420:android:e2f7519aadda9bf530f199",
+};
