b6c49fe969d3227122815b685a93515f7dfe6cd1
Sakura Wallace <066sakura@gmail.com>
2025-07-19 14:53:22 +0100

    updated temporary onboarding, sign in and home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import tw from "twrnc";
 
-export default function Home() {
+export default function HomeScreen() {
     return (
         <View style={tw`flex-1 items-center justify-center bg-white`}>
             <Text style={tw`text-2xl font-bold`}>Home Screen</Text>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,26 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
+import { RootStackParamList } from "../navigation/RootNavigator";
 
-export default function SignIn({ navigation }: any) {
+type Props = NativeStackScreenProps<RootStackParamList, "SignIn">;
+
+export default function SignInScreen({ navigation }: Props) {
     return (
         <View style={tw`flex-1 items-center justify-center bg-white`}>
-            <Text style={tw`text-2xl font-bold`}>Sign In</Text>
+            <Text style={tw`text-2xl font-bold mb-5`}>Sign In</Text>
+            <TouchableOpacity style={tw`w-60 px-5 py-3 bg-black rounded-lg mb-3`}>
+                <Text style={tw`text-white text-center`}>Sign in with Apple</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={tw`w-60 px-5 py-3 bg-red-500 rounded-lg`}>
+                <Text style={tw`text-white text-center`}>Sign in with Google</Text>
+            </TouchableOpacity>
             <TouchableOpacity
-                style={tw`mt-5 px-5 py-3 bg-blue-500 rounded-lg`}
+                style={tw`mt-8 px-5 py-3 bg-blue-500 rounded-lg`}
                 onPress={() => navigation.replace("Home")}
             >
-                <Text style={tw`text-white text-base`}>Go to Home</Text>
+                <Text style={tw`text-white text-center`}>Go to Home</Text>
             </TouchableOpacity>
         </View>
     );
