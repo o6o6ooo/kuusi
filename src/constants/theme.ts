2aedd9e0365ba912f5282aef251c80cf28565e61
Sakura Wallace <066sakura@gmail.com>
2025-07-19 15:25:59 +0100

    created theme.ts

---
+++ src/constants/theme.ts
@@ -0,0 +1,15 @@
+export const LightTheme = {
+    background: '#ffffff',
+    text: '#2A3140',
+    primary: '#5C9BD1',
+    secondary: '#A5C3DE',
+    card: '#f9fafb',
+};
+
+export const DarkTheme = {
+    background: '#1E2633',
+    text: '#f3f4f6',
+    primary: '#607cac',
+    secondary: '#A5C3DE',
+    card: '#2A3140',
+};
