d5c59c24d2c00f7fa8bd2fae402fcde42b0cfc86
Sakura Wallace <066sakura@gmail.com>
2025-07-19 14:36:25 +0100

    updated RootNavigator

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,16 +1,23 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Onboarding from '../screens/Onboarding';
-import SignIn from '../screens/SignIn';
-import TabNavigator from './TabNavigator';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import Home from "../screens/Home";
+import Onboarding from "../screens/Onboarding";
+import SignIn from "../screens/SignIn";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+    Onboarding: undefined;
+    SignIn: undefined;
+    Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Onboarding" component={Onboarding} />
             <Stack.Screen name="SignIn" component={SignIn} />
-            <Stack.Screen name="Main" component={TabNavigator} />
+            <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
     );
 }
