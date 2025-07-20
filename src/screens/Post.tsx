125eb6afba47813549b3fa82fd3360d5d1e1bd89
Sakura Wallace <066sakura@gmail.com>
2025-07-20 19:48:58 +0100

    applied theme to all pages

--- src/screens/Post.tsx
+++ src/screens/Post.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, useColorScheme } from "react-native";
 import tw from "twrnc";
+import { DarkTheme, LightTheme } from "../constants/theme";
 
 export default function Post() {
+    const colorScheme = useColorScheme();
+    const theme = colorScheme === "dark" ? DarkTheme : LightTheme;
+
     return (
-        <View style={tw`flex-1 items-center justify-center bg-white`}>
-            <Text style={tw`text-2xl font-bold`}>Post Screen</Text>
+        <View style={[tw`flex-1 items-center justify-center`, { backgroundColor: theme.background }]}>
+            <Text style={[tw`text-2xl font-bold`, { color: theme.text }]}>Post</Text>
         </View>
     );
 }
