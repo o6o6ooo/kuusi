1bc45d8a1cb3dbb318176768e2fa3b00498fa8d7
Sakura Wallace <066sakura@gmail.com>
2025-07-20 23:29:47 +0100

    added iOS and Android client id

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,1 +1,3 @@
 export const GOOGLE_WEB_CLIENT_ID = "475913927226-bsk6cej41gj5a3d805gjk6m6ucrv3jmp.apps.googleusercontent.com";
+export const GOOGLE_IOS_CLIENT_ID = "475913927226-vsnqope3j1pbjq6m6t9qeo4anouu20o5.apps.googleusercontent.com";
+export const GOOGLE_ANDROID_CLIENT_ID = "475913927226-sk02msl8jfu4j6svb4nuqc03fca2gt10.apps.googleusercontent.com";
