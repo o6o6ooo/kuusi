36ebd974fca344d9dbb028c50afa3428b076d9f6
Sakura Wallace <066sakura@gmail.com>
2025-07-25 18:03:45 +0100

    added firebase web client to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { DefaultTheme, NavigationContainer, DarkTheme as NavigationDarkTheme } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
 import { DarkTheme, LightTheme } from './src/constants/theme'; // テーマ定義をインポート
@@ -8,6 +9,10 @@ export default function App() {
     const theme = scheme === 'dark'
         ? { ...NavigationDarkTheme, colors: { ...NavigationDarkTheme.colors, ...DarkTheme } }
         : { ...DefaultTheme, colors: { ...DefaultTheme.colors, ...LightTheme } };
+
+    GoogleSignin.configure({
+        webClientId: '898290307420-93fv03fvvvh5kd5lfmqk4a0at34dek89.apps.googleusercontent.com',
+    })
 
     return (
         <NavigationContainer theme={theme}>
