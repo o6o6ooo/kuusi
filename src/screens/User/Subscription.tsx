77d010da2b8d38dfe15d4cb67a604765d83cb8b4
Sakura Wallace <066sakura@gmail.com>
2025-07-27 04:39:34 +0100

    added subscribe buttons

--- src/screens/User/Subscription.tsx
+++ src/screens/User/Subscription.tsx
@@ -1,13 +1,16 @@
 import { getAuth } from "@react-native-firebase/auth";
-import firestore from "@react-native-firebase/firestore";
-import { Check } from "phosphor-react-native";
+import firestore, { Timestamp } from "@react-native-firebase/firestore";
+import { format } from "date-fns";
+import { Calendar, CalendarDots, Check } from "phosphor-react-native";
 import React, { useEffect, useState } from "react";
-import { Text, useColorScheme, View } from "react-native";
+import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
 import tw from "twrnc";
 import { DarkTheme, LightTheme } from "../../constants/theme";
 
 export default function Subscription() {
-    const [isPremium, setIsPremium] = useState(false);
+    const [user, setUser] = useState<{ premium: boolean; plan?: string; nextBillingDate?: Timestamp }>({
+        premium: false,
+    });
     const colorScheme = useColorScheme();
     const theme = colorScheme === "dark" ? DarkTheme : LightTheme;
 
@@ -18,7 +21,12 @@ export default function Subscription() {
 
             const doc = await firestore().collection("users").doc(uid).get();
             if (doc.exists()) {
-                setIsPremium(doc.data()?.premium === true);
+                const data = doc.data();
+                setUser({
+                    premium: data?.premium === true,
+                    plan: data?.plan ?? "",
+                    nextBillingDate: data?.nextBillingDate || undefined,
+                });
             }
         };
 
@@ -34,7 +42,7 @@ export default function Subscription() {
                 "Create up to 3 groups",
                 "Invite up to 5 users",
             ],
-            selected: !isPremium,
+            selected: !user.premium,
         },
         {
             name: "Premium",
@@ -44,14 +52,20 @@ export default function Subscription() {
                 "Create up to 10 groups",
                 "Invite up to 20 users",
             ],
-            selected: isPremium,
+            selected: user.premium,
         },
     ];
 
     return (
         <View>
             {plans.map((plan) => (
-                <View key={plan.name} style={[tw`mb-2 p-2 rounded-xl flex-row`, { backgroundColor: theme.card }]}>
+                <View
+                    key={plan.name}
+                    style={[
+                        tw`mb-2 p-2 rounded-xl flex-row`,
+                        { backgroundColor: theme.card },
+                    ]}
+                >
                     <View style={tw`w-1/5 justify-center items-center pt-1`}>
                         {plan.selected && (
                             <Check size={20} color={theme.text} weight="bold" />
@@ -59,14 +73,61 @@ export default function Subscription() {
                     </View>
 
                     <View style={tw`w-4/5`}>
-                        <Text style={[tw`font-semibold mb-1`, { color: theme.text }]}>
+                        <Text
+                            style={[tw`font-semibold mb-1`, { color: theme.text }]}
+                        >
                             {plan.name}
                         </Text>
                         {plan.features.map((feature, idx) => (
-                            <Text key={idx} style={[tw`text-xs`, { color: theme.text }]}>
+                            <Text key={idx} style={[tw`text-xs leading-4`, { color: theme.text }]}>
                                 • {feature}
                             </Text>
                         ))}
+
+                        {plan.name === "Premium" && (
+                            user.premium ? (
+                                <View style={tw`mt-3`}>
+                                    <Text style={[tw`text-xs`, { color: theme.text }]}>Your plan:{' '}
+                                        <Text style={[tw`text-xs font-medium`, { color: theme.primary }]}>{user.plan}</Text>
+                                    </Text>
+                                    <Text style={[tw`text-xs`, { color: theme.text }]}>Next billing date:{' '}
+                                        <Text style={[tw`text-xs font-medium`, { color: theme.primary }]}>{user.nextBillingDate ? format(user.nextBillingDate.toDate(), "dd MMM yyyy") : "--"}</Text>
+                                    </Text>
+                                </View>
+                            ) : (
+                                <View style={tw`flex-row mt-3`}>
+                                    <TouchableOpacity
+                                        style={[
+                                            tw`flex-1 flex-row items-center mr-2 py-2 rounded-lg`,
+                                            { backgroundColor: theme.background },
+                                        ]}
+                                    >
+                                        <View style={tw`w-8 items-center`}>
+                                            <CalendarDots size={18} color={theme.text} />
+                                        </View>
+                                        <View>
+                                            <Text style={[tw`text-xs`, { color: theme.text }]}>Pay monthly</Text>
+                                            <Text style={[tw`text-xs`, { color: theme.text }]}>£5.00 / month</Text>
+                                        </View>
+                                    </TouchableOpacity>
+
+                                    <TouchableOpacity
+                                        style={[
+                                            tw`flex-1 flex-row items-center py-2 rounded-lg`,
+                                            { backgroundColor: theme.background },
+                                        ]}
+                                    >
+                                        <View style={tw`w-8 items-center`}>
+                                            <Calendar size={18} color={theme.text} />
+                                        </View>
+                                        <View>
+                                            <Text style={[tw`text-xs`, { color: theme.text }]}>Pay annually</Text>
+                                            <Text style={[tw`text-xs`, { color: theme.text }]}>£50.00 / year</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            )
+                        )}
                     </View>
                 </View>
             ))}
