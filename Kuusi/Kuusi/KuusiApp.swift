d0d4ad7d7d9958259e4430bbb6d7d6725a395758
Sakura Wallace <066sakura@gmail.com>
2026-03-06 19:02:45 +0000

    feat: add initial swiftui project

---
+++ Kuusi/Kuusi/KuusiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuusiApp.swift
+//  Kuusi
+//
+//  Created by Sakura Wallace on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct KuusiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
