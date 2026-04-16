915728e84a2a3177f18968189637cf3ae72b84dc
Sakura Wallace <066sakura@gmail.com>
2026-04-16 11:34:38 +0100

    feat: add firebase app check setup

--- Kuusi/Kuusi/AppDelegate.swift
+++ Kuusi/Kuusi/AppDelegate.swift
@@ -1,11 +1,27 @@
+import FirebaseAppCheck
 import FirebaseCore
 import UIKit
+
+private final class KuusiAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+#if DEBUG
+        return AppCheckDebugProviderFactory().createProvider(with: app)
+#else
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app)
+        } else {
+            return DeviceCheckProvider(app: app)
+        }
+#endif
+    }
+}
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
+        AppCheck.setAppCheckProviderFactory(KuusiAppCheckProviderFactory())
         FirebaseApp.configure()
         return true
     }
